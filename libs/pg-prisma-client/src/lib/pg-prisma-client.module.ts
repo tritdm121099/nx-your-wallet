af26913536a94aa202624c733f6d35968574aac2
tri.tdm <tritruong121099@gmail.com>
2024-10-15 21:57:02 +0700

    [feat] re-config prisma client module

--- libs/pg-prisma-client/src/lib/pg-prisma-client.module.ts
+++ libs/pg-prisma-client/src/lib/pg-prisma-client.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PgPrismaClientService } from './pg-prisma-client.service';
 
+@Global()
 @Module({
   providers: [PgPrismaClientService],
   exports: [PgPrismaClientService],
