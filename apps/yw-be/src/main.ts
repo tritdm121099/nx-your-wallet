c068482a0b7c37e6abaa87152172b54dd4c31083
tri.tdm <tritruong121099@gmail.com>
2024-10-20 18:03:42 +0700

    [feat] setup cookie parse for be app

--- apps/yw-be/src/main.ts
+++ apps/yw-be/src/main.ts
@@ -7,12 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
+  app.use(cookieParser());
   app.enableCors();
 
   const port = process.env.PORT || 3000;
--- libs/pg-prisma-client/src/lib/pg-prisma-client.service.ts
+++ libs/pg-prisma-client/src/lib/pg-prisma-client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma-clients/pg-prisma';
+import { PrismaClient } from '@prisma/pg-prisma-clients';
 
 @Injectable()
 export class PgPrismaClientService extends PrismaClient {
