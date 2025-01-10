698541ade21c5539d39d63da788915b5343812b9
tri.tdm <tritruong121099@gmail.com>
2025-01-10 23:54:59 +0700

    [feat] switch tailwind themes

--- libs/client/shell/feature/src/lib/themes/themes.service.ts
+++ libs/client/shell/feature/src/lib/themes/themes.service.ts
@@ -1,8 +1,8 @@
-import { computed, inject, Injectable, signal, Signal } from "@angular/core";
-import { oLocalStorageService } from "../services/local-storage.service";
-import { Theme } from "./themes.i";
+import { computed, inject, Injectable, signal, Signal } from '@angular/core';
+import { oLocalStorageService } from '../services/local-storage.service';
+import { Theme } from './themes.i';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ThemesService {
   keyStorage = 'yw-theme';
 
@@ -12,20 +12,37 @@ export class ThemesService {
   isDarkTheme: Signal<boolean> = computed(() => {
     switch (this.theme()) {
       case 'light':
+        this.updateTheme('light');
         return false;
       case 'dark':
+        this.updateTheme('dark');
         return true;
-      case 'system':
-        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+      case 'system': {
+        const isDark = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        ).matches;
+        this.updateTheme(isDark ? 'dark' : 'light');
+
+        return isDark;
+      }
     }
   });
 
   get themeStorage(): Theme {
-    return <Theme> this.localStorage.getItem(this.keyStorage) || 'system';
+    return <Theme>this.localStorage.getItem(this.keyStorage) || 'system';
   }
 
   changeTheme(change: Theme): void {
     this.localStorage.setItem(this.keyStorage, change);
     this.theme.set(change);
+  }
+
+  private updateTheme(theme: 'light' | 'dark'): void {
+    const body = document.body;
+    if (theme === 'dark') {
+      body.classList.add('dark');
+    } else {
+      body.classList.remove('dark');
+    }
   }
 }
