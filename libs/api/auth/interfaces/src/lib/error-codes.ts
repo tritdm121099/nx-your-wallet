dacb6f68b245d6cc911862073e4201dd809187ab
tri.tdm <tritruong121099@gmail.com>
2025-02-08 13:33:44 +0700

    [feat] add libs to share interface fe and be auth feature

---
+++ libs/api/auth/interfaces/src/lib/error-codes.ts
@@ -0,0 +1,9 @@
+export enum LoginErrorCodes {
+  AuthFailed = 'auth-failed',
+  TryOtherMethod = 'try-other-method',
+}
+
+export enum RegisterErrorCodes {
+  ConfirmPassFail = 'confirm-pass-fail',
+  EmailHaveUsed = 'email-have-used',
+}
